47877cb1ff7f1610a3ee51837a4ea4ffd67993cb
dakshbhatt21 <dakshbhatt21@gmail.com>
2022-09-16 19:03:31 +0100

    prevent api calls while navigation

--- app/src/main/java/com/composepagingdemo/MainViewModel.kt
+++ app/src/main/java/com/composepagingdemo/MainViewModel.kt
@@ -1,9 +1,11 @@
 package com.composepagingdemo
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
@@ -13,7 +15,9 @@ class MainViewModel @Inject constructor(
     private val postSource: PostSource
 ) : ViewModel() {
 
-    val posts: Flow<PagingData<Post>> = Pager(PagingConfig(pageSize = 10)) { postSource }.flow
+    val posts: Flow<PagingData<Post>> = Pager(PagingConfig(pageSize = 10)) { postSource }
+        .flow   // convert to Flow
+        .cachedIn(viewModelScope) // cache data in viewmodel scope to avoid frequent api calls
 
     var post: Post? = null
 }
